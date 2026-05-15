5a2be62c10a43bf2cf40f6b9918fdf64cf0b6bff
Rayhan-2005 <132483187+Rayhan-2005@users.noreply.github.com>
2026-05-15 21:17:33 +0800

    setup: hardhat project with monad testnet config

---
+++ hardhat.config.ts
@@ -0,0 +1,15 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    monad: {
+      url: "https://testnet-rpc.monad.xyz",
+      chainId: 10143,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
